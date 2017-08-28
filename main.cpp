587e59e06d1711bd92e20c3514c9136fb7c5a770
erbth <t.erbesdobler@team103.com>
2017-08-28 18:27:33 +0200

    Applied Apache License, Version 2.0

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+/** This file is part of dirCompare
+ *
+ * Copyright 2017 Thomas Erbesdobler <t.erbesdobler@team103.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <iostream>
 #include <vector>
 #include <memory>
@@ -88,19 +105,19 @@ static int dirCompare(int argc, char** argv)
 	}
 	else
 	{
-		cout << endl << "ignored files:" << endl;
+		*(sp->getLog()) << endl << "ignored files:" << endl;
 		for (auto f : sp->getIgnoreFiles())
 		{
-			cout << f << endl;
+			*(sp->getLog()) << f << endl;
 		}
 
-		cout << endl << "ignored directories:" << endl;
+		*(sp->getLog()) << endl << "ignored directories:" << endl;
 		for (auto d : sp->getIgnoreDirectories())
 		{
-			cout << d << endl;
+			*(sp->getLog()) << d << endl;
 		}
 
-		cout << endl;
+		*(sp->getLog()) << endl;
 
 		auto csf = createComparisonStrategyFactory(sp);
 
